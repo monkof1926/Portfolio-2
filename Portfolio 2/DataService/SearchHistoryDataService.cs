450c185f1a19246d4b81b23326b5b9d2d0f81350
Butterhund <nikolkj@ruc.dk>
2022-11-16 18:24:16 +0100

    Fixed DataService for SearchHistory

--- Portfolio 2/DataService/SearchHistoryDataService.cs
+++ Portfolio 2/DataService/SearchHistoryDataService.cs
@@ -5,10 +5,77 @@ using System.Text;
 using System.Threading.Tasks;
 using DataLayer.Domain;
 using DataLayer.IDataService;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataLayer.DataService
 {
     public class SearchHistoryDataService : ISearchHistoryDataService
     {
+        public void CreateSearchHistory(SearchHistory searchHistory)
+        {
+            using var db = new NorthwindContext();
+            db.searchhistory.Add(searchHistory);
+            db.SaveChanges();
+        }
+        public bool DeleteSearchHistory(int? searchOrder)
+        {
+            using var db = new NorthwindContext();
+            var search = db.searchhistory.Find(searchOrder);
+            if (search != null)
+            {
+                db.searchhistory.Remove(search);
+            }
+            else { return false; }
+
+            return db.SaveChanges() > 0;
+        }
+        public IList<SearchHistory> GetSearchHistories(int page, int pageSize)
+
+        {
+            using var db = new NorthwindContext();
+            return db.searchhistory
+                .Include(x => x.searchWord)
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderBy(x => x.searchOrder)
+                .ToList();
+        }
+        public SearchHistory? GetSearchHistories(int? searchOrder)
+        {
+            using var db = new NorthwindContext();
+            if (searchOrder != null)
+            {
+                return db.searchhistory.Find(searchOrder);
+            }
+            return null;
+        }
+        public bool UpdateSearchHistory(SearchHistory searchHistoryM)
+        {
+            using var db = new NorthwindContext();
+            var dbSearch = db.searchhistory.Find(searchHistoryM.searchOrder);
+            if (dbSearch == null) return false;
+            dbSearch.searchOrder = searchHistoryM.searchOrder;
+            dbSearch.searchWord = searchHistoryM.searchWord;
+            db.SaveChanges();
+            return true;
+        }
+        public int GetNumberOfSearchHistories()
+        {
+            using var db = new NorthwindContext();
+            return db.searchhistory.Count();
+        }
+        public IList<SearchHistorySearchModel> GetSearchHistoryByUser(string search)
+        {
+            using var db = new NorthwindContext();
+            return db.searchhistory
+                .Include(x => x.searchWord)
+                .Where(x => x.searchWord == search)
+                .Select(x => new SearchHistorySearchModel
+                {
+                    searchSearch = x.searchWord
+                })
+                .ToList();
+        }
     }
 }
--- Portfolio 2/Models/SearchHistorySearchModel.cs
+++ Portfolio 2/Models/SearchHistorySearchModel.cs
@@ -8,6 +8,7 @@ namespace DataLayer.Models
 {
     public class SearchHistorySearchModel
     {
+        public string? searchSearch { get; set; }
         public string? searchWord { get; set; }
         public int? searchOrder { get; set; }
         //Add public string? searchUserID { get; set; }

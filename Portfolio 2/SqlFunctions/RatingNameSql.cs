c7bcfbf111c21e602e82f4ef24b8087a848c1fee
Butterhund <nikolkj@ruc.dk>
2022-11-16 21:21:53 +0100

    Added a new folder (SqlFunctions) and two new classes (RatingNameSql, RatingTitleSql)

---
+++ Portfolio 2/SqlFunctions/RatingNameSql.cs
@@ -0,0 +1,33 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio_2.SqlFunctions
+{
+    public class RatingNameSql
+    {
+        static void UseEntityFrameworkToCallFunction(string connectionString)
+        {
+            Console.WriteLine("Call function from Entity Framework");
+            using var ctx = new NorthwindContex(connectionString);
+
+            int id = 101;
+            string name = "testing";
+            string description = "testing desc";
+
+            ctx.Database.ExecuteSqlInterpolated($"select insertcategory({id},{name},{description})");
+
+            var category = ctx.Categories.Find(id);
+
+            Console.WriteLine("Newly inserted category:");
+            Console.WriteLine($"Id={category.Id}, Name={category.Name}, Description={category.Description}");
+
+            ctx.Categories.Remove(category);
+
+            ctx.SaveChanges();
+        }
+    }
+}

da4ba5459be24349343e114768d8a9d86cd2aa63
monkof1926 <l_bjacobsen@hotmail.com>
2022-12-17 17:10:28 +0100

    change best match with ordered by

--- Portfolio 2/SqlFunctions/BestMatchSearchFunc.cs
+++ Portfolio 2/SqlFunctions/BestMatchSearchFunc.cs
@@ -17,23 +17,27 @@ namespace DataLayer.SqlFunctions
                 // Type 3 is Best match search 
                 if (type == 3 && searchwords.Length > 3)
                 {
-                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},{searchwords[1]},{searchwords[2]},{searchwords[3]})").Skip(page * pageSize).Take(pageSize).ToList();
+                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},{searchwords[1]},{searchwords[2]},{searchwords[3]})")
+                                                                        .Skip(page * pageSize)
+                                                                        .Take(pageSize)
+                                                                        .OrderBy(x => x.title)
+                                                                        .ToList();
 
                 }
                 if (type == 3 && searchwords.Length == 1)
                 {
                     
-                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},' ',' ',' ')").Skip(page * pageSize).Take(pageSize).ToList();
+                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},' ',' ',' ')").Skip(page * pageSize).Take(pageSize).OrderBy(x => x.title).ToList();
 
                 }
                  if (type == 3 && searchwords.Length == 2)
                 {
-                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},{searchwords[1]},' ',' ')").Skip(page * pageSize).Take(pageSize).ToList();
+                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},{searchwords[1]},' ',' ')").Skip(page * pageSize).Take(pageSize).OrderBy(x => x.title).ToList();
 
                 }
                  if (type == 3 && searchwords.Length == 3)
                 {
-                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},{searchwords[1]},{searchwords[2]},' ')").Skip(page * pageSize).Take(pageSize).ToList();
+                    return ctx.rankQuerySearchResults.FromSqlInterpolated($"select * from best_match_search({searchwords[0]},{searchwords[1]},{searchwords[2]},' ')").Skip(page * pageSize).Take(pageSize).OrderBy(x => x.title).ToList();
 
                 }
             }
